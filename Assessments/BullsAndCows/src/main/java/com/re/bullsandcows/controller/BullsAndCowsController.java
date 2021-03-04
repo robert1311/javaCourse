70b8c75364548b0f745e840589970cf4980d7ce9
Robert Esquivel <roberte1311@gmail.com>
2021-03-04 15:15:29 -0600

    Complete Bulls and Cows game with Back End REST Service Only. Use Postman to view JSON responses and to send Httprequests. EndPoint instructions on BullsAndCowsController.java file

--- Assessments/BullsAndCows/src/main/java/com/re/bullsandcows/controller/BullsAndCowsController.java
+++ Assessments/BullsAndCows/src/main/java/com/re/bullsandcows/controller/BullsAndCowsController.java
@@ -33,6 +33,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/bullsAndCows")
 public class BullsAndCowsController {
     
+    /* A Game should have an answer and a status (in progress or finished). 
+    While the game is in progress, users should not be able to see the answer. 
+    The answer will be a 4-digit number with no duplicate digits.
+
+Each Round will have a guess, the time of the guess, and the result of the guess 
+    in the format "e:0:p:0" where "e" stands for exact matches and "p" stands 
+    for partial matches.
+
+You will need several REST endpoints for this:
+
+"begin" - POST – Starts a game, generates an answer, and sets the correct 
+    status. Should return a 201 CREATED message as well as the created gameId.
+"guess" – POST – Makes a guess by passing the guess and gameId in as JSON. The 
+    program must calculate the results of the guess and mark the game finished 
+    if the guess is correct. It returns the Round object with the results 
+    filled in.
+"game" – GET – Returns a list of all games. Be sure in-progress games do not 
+    display their answer.
+"game/{gameId}" - GET – Returns a specific game based on ID. Be sure in-progress 
+    games do not display their answer.
+"rounds/{gameId} – GET – Returns a list of rounds for the specified game sorted 
+    by time. */
+    
     @Autowired
     BullsAndCowsServiceLayer service;
     
